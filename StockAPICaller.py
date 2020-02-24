ff759b1ef13f816cda06eb3db5b97bb5f3e7b2e7
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-02-24 00:11:34 +0000

    Add files via upload

--- StockAPICaller.py
+++ StockAPICaller.py
@@ -1,4 +1,12 @@
-# API KEY = 0OL6B1YZZABY32OD
 # https://www.alphavantage.co/support/#api-key - API Source
 import requests
-
+import json
+apikey = '0OL6B1YZZABY32OD'
+datatype = 'json'
+outputsize = 'full'
+interval = '1min'
+symbol = 'MSFT'  # tick of company
+function = 'TIME_SERIES_INTRADAY'
+url = "https://www.alphavantage.co/query?function="+function+"&symbol="+symbol+"&interval="+interval+"&apikey="+apikey
+call =requests.get(url).json()
+print(call)
