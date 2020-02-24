56cd695cb2faf309149432ac7c3e1e14d18e6637
makiisthenes <52138450+makiisthenes@users.noreply.github.com>
2020-02-24 00:34:33 +0000

    Add files via upload

---
+++ wordMachineLearning.py
@@ -0,0 +1,3 @@
+# This will be the main program that decides whether or not a company is worth investing in or not...
+import os
+# https://www.lexalytics.com/lexablog/machine-learning-natural-language-processing
